c7463bfd24c2294711975090ac4e7c2de9b56b6f
jhonronell <jhonronell@gmail.com>
2015-07-04 10:40:23 +0800

    added factory layer

---
+++ src/main/java/com/jronell/factory/ServiceFactory.java
@@ -0,0 +1,17 @@
+package com.jronell.factory;
+
+import com.jronell.dao.impl.EventDaoImpl;
+import com.jronell.service.EventService;
+import com.jronell.serviceImpl.EventServiceImpl;
+
+public class ServiceFactory {
+    public static EventService createEventService() {
+    
+    	EventServiceImpl eventService = new EventServiceImpl();
+        eventService.setEventDao(new EventDaoImpl());
+         return eventService;
+    }
+}
+
+//the service factory create a type from interface of the eventservice then return the 
+//event implementation 
